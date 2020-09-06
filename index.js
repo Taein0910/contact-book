dcbc3fbfbc63403bef0656b14a85cc1394c97215
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-09-06 16:05:11 +0900

    first-commit

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGO_DB);
+mongoose.connect("mongodb+srv://dbuser:yti050910@contact-book.7j3c6.mongodb.net/");
 var db = mongoose.connection;
-db.once('open', function(){
-  console.log('DB connected');
+db.once('open', function() {
+    console.log('DB connected');
 });
-db.on('error', function(err){
-  console.log('DB ERROR : ', err);
+db.on('error', function(err) {
+    console.log('DB ERROR : ', err);
 });
 
 // Other settings
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 // Routes
@@ -31,6 +31,6 @@ app.use('/contacts', require('./routes/contacts'));
 
 // Port setting
 var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+app.listen(port, function() {
+    console.log('server on! http://localhost:' + port);
 });
